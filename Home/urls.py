6931dc6e926acac04e40a0d341c1b432034f1328
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-09-22 22:24:43 -0600

    Parte publica

--- Home/urls.py
+++ Home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import inicio_view, SesionView
+from .views import inicio_view, SesionView, AcercaView, ContactoView
 
 urlpatterns = [
     path('', inicio_view, name='inicio'),  
     path('sesion/', SesionView.as_view(), name='sesion'),  
+    path('about/', AcercaView.as_view(), name='about'),
+    path('contact/', ContactoView.as_view(), name='contact'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -9,4 +9,9 @@ def inicio_view(request):
 class SesionView(LoginRequiredMixin, TemplateView):
     template_name = 'sesion.html'
     login_url = reverse_lazy('login')
-    
+
+class AcercaView(TemplateView):
+    template_name = 'acerca.html'
+
+class ContactoView(TemplateView):
+    template_name = 'contacto.html'
