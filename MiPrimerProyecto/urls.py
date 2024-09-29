c6561ae108149de9eff630656418170e113f44d8
Katlin-Chapa <kchapal@miumg.edu.gt>
2024-09-28 22:18:46 -0600

    Tarea terminada

--- MiPrimerProyecto/urls.py
+++ MiPrimerProyecto/urls.py
@@ -18,12 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-from Catalogo.views import PaisViewSet, DepartamentoViewSet
+from Catalogo.views import PaisViewSet, DepartamentoViewSet, MunicipioViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pais', PaisViewSet)
 router.register(r'departamento', DepartamentoViewSet)
-
+router.register(r'municipio', MunicipioViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
